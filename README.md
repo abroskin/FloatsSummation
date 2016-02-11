392c0e4774e22c5c567572d860ac435b358707a4
Alexandr Abroskin <alexandr.abroskin@here.com>
2016-02-11 20:04:18 +0100

    Added algorithm.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FloatsSummation
 Simple algorithm for summing floats.
+
+# Details
+Effectively works only with not negative values.
+Modifies an input vector.
+
+# Algorithm
+Creates a heap on an input vector.
+On each iteration pops two lowest numbers, sums them and push result back to the heap.
+
+# Complexity
+Runtime complexity - O(log n)
+Memory complexity - O(1)
---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+template <class T>
+bool comp(const T& a, const T& b)
+{
+    return b < a;
+}
+
+template < class T >
+T sum(std::vector<T>& v)
+{
+    if (v.empty()) {
+        return static_cast<T>(0.0);
+    } else if (v.size() == 1u) {
+        return v.front();
+    }
+
+    std::make_heap(v.begin(),v.end(), comp<T>);
+
+    while(v.size() > 1u) {
+        std::pop_heap( v.begin(), v.end(), comp<T> );
+        std::pop_heap( v.begin(), v.end() - 1u, comp<T> );
+        v[ v.size() - 2u ] += v.back();
+        v.pop_back();
+        std::push_heap( v.begin(), v.end(), comp<T> );
+    }
+
+    return v.front();
+}
+
+int main()
+{
+    std::vector<double> test_d;
+    std::cout << sum( test_d ) << std::endl;
+
+    test_d.push_back(0.0);
+    std::cout << sum( test_d ) << std::endl;
+
+    test_d.push_back(0.0);
+    std::cout << sum( test_d ) << std::endl;
+
+    std::vector<float> test_f{10.0f, 0.0f, 5.0f, 5.0f};
+    std::cout << sum( test_f ) << std::endl;
+
+    return 0;
+}
+
